e2c8b6e65bba095070817fbc3bc8ea511f88f838
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2023-04-22 11:35:57 -0300

    up

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL_SERVER: "http://localhost:3000/api/",
+    URL_SERVER: "https://localhost:3000/api/",
     SECRET_KEY_SERVER: "H9vLGUXfVErDcSyUu6pKF2jdkANZsqvHt7KcifD8DaiwZ",
     NEXTAUTH_SECRET: "32b53b9195368fbcc4fc92e58b7a7028",
     DB_HOST: "89.116.214.20",
