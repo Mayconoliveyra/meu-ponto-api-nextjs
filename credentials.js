f83e223e887f83934229d417ffd301d26bfc958c
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2023-04-22 12:28:19 -0300

    aa

--- credentials.js
+++ credentials.js
@@ -1,9 +1,5 @@
 module.exports = {
     URL_SERVER: "http://localhost:3000/api/",
     SECRET_KEY_SERVER: "H9vLGUXfVErDcSyUu6pKF2jdkANZsqvHt7KcifD8DaiwZ",
-    NEXTAUTH_SECRET: "32b53b9195368fbcc4fc92e58b7a7028",
-    DB_HOST: "89.116.214.20",
-    DB_USER: "softconnect",
-    DB_PASSWORD: "T5NxEaecgntJ7hP!mP7ZRx@aa",
-    DB_PORT: 3306
+    NEXTAUTH_SECRET: "c7b0478d2f5d30298bfe805f55fc6586",
 }
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,15 @@
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT } = require("./credentials")
+const { loadEnvConfig } = require('@next/env')
+const dev = process.env.NODE_ENV !== 'production'
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = loadEnvConfig('./', dev).combinedEnv
+
 module.exports = {
     client: 'mysql2',
     connection: {
         host: DB_HOST,
         user: DB_USER,
         password: DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        database: DB_DATABASE,
         port: DB_PORT,
         dateStrings: true,
     },
