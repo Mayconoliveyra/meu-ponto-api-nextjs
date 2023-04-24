1695a27f5529a51f201611c4b5836bc9a2429e8a
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2023-04-24 12:37:51 -0300

    up

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -265,14 +265,7 @@ export default function Dashboard({ session, pDiario }) {
     );
 }
 
-export async function loadPosts() {
-    // Call an external API endpoint to get posts
-    const res = await fetch("http://127.0.0.1:3000/api/pontos?_diario=true");
-    const data = await res.json()
-
-    return data
-}
-
+import { loadPosts } from '../../lib/load-posts'
 export async function getServerSideProps(context) {
     const { req } = context
     const session = await getSession({ req })
