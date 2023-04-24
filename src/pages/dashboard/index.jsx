2b62bab6dbb3954aa860f9883539856d63e0ab11
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2023-04-24 12:27:42 -0300

    up

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -265,16 +265,22 @@ export default function Dashboard({ session, pDiario }) {
     );
 }
 
+export async function loadPosts() {
+    // Call an external API endpoint to get posts
+    const res = await fetch("http://127.0.0.1:3000/api/pontos?_diario=true");
+    const data = await res.json()
+
+    return data
+}
+
 export async function getServerSideProps(context) {
     const { req } = context
     const session = await getSession({ req })
     if (session && session.id) {
-        const response = await fetch("http://127.0.0.1:3000/api/pontos?_diario=true");
-        const pDiario = await response.json();
+        const pDiario = await loadPosts()
 
-
-      /*   const axios = await apiServer(session);
-        const pDiario = await axios.get("pontos?_diario=true").then((res) => res.data).catch(error => console.log(error)) */
+        /*   const axios = await apiServer(session);
+          const pDiario = await axios.get("pontos?_diario=true").then((res) => res.data).catch(error => console.log(error)) */
 
         return {
             props: { session, pDiario },
