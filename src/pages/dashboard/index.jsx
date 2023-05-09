735a100a1ecda6835c9260aa78c811b77286d359
Maycon Oliveyra <63799417+Mayconoliveyra@users.noreply.github.com>
2023-05-09 19:03:00 -0300

    uo

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -206,7 +206,9 @@ export default function AdmDashboard({ session, pDiario }) {
 
     const teste = async () => {
         console.log("teste")
-        await axios.get("www.softconnectdeveloper.shop:3030/token")
+        axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+        axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+        await axios.get("http://softconnectdeveloper.shop:3030/token")
             .then(res => console.log(res.data))
             .catch(err => console.log(err))
 
